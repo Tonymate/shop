0797b8dac2d133b2218cb21a1b007690722d9458
Raze Viana <razeviana@hotmail.com>
2020-02-11 13:28:47 +0000

    fixed nav bar alignment and added sign in/up links

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,14 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light"> <!--NAV BAR-->
-    <a class="navbar-brand text-light mx-5" href="/"><img src="img/logo.png" alt="OPPC Brand Logo" height="40" class="img-responsive logo"/></a>
+    <a class="navbar-brand text-light mx-2" href="/"><img src="img/logo.png" alt="OPPC Brand Logo" height="40" class="img-responsive logo"/></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav " >
-          <a class="nav-item nav-link text-dark mx-2" href="#">HOME</a>
-          <a class="nav-item nav-link text-dark mx-2" href="#">FEATURES</a>
-          <a class="nav-item nav-link text-dark mx-2" href="#">PRICING</a>
+        <div class="navbar-nav  mx-2" >
+          <a class="nav-item nav-link text-dark" href="#">HOME</a>
+          <a class="nav-item nav-link text-dark" href="#">FEATURES</a>
+          <a class="nav-item nav-link text-dark" href="#">PRICING</a>
         </div>
-      </div>
+
+        <div class="navbar-nav ml-auto mx-3" >
+          <a class="nav-item nav-link text-dark" href="#">SIGN UP</a>
+          <a class="nav-item nav-link text-dark" href="#">SIGN IN</a>
+        </div>    
+    </div>
 </nav>
